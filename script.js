81c8ca5a945f7d8f666434f229d947cd5e999c37
Həşimli Ədalət <86829581+EdaletHashimli@users.noreply.github.com>
2022-04-26 17:38:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,16 @@ let itemright = document.querySelectorAll('.item_2')
 let input_text = document.querySelector('.input-text');
 let output_text = document.querySelector('.output-text');
 
-
-
-
 let mez = 'RUB';
 let mezsag = 'USD';
 inputs.addEventListener('keyup', function () {
-
-
-
+  
   getValueLeft();
 })
 outputs.addEventListener('keyup', function () {
 
   getValueRight();
 })
-
-
-
-
-
-
 
 itemleft.forEach((item) => {
   item.addEventListener('click', function () {
@@ -45,9 +34,6 @@ itemleft.forEach((item) => {
   });
 })
 
-
-
-
 itemright.forEach((item) => {
   item.addEventListener('click', function () {
     itemright.forEach((item) => {
@@ -61,11 +47,10 @@ itemright.forEach((item) => {
 itemright.forEach((item) => {
   item.addEventListener('click', function () {
     mezsag = this.innerText;
-    getValueLeft()
+    getValueRight()
 
   });
 })
-
 
 const getValueLeft = async () => {
 
@@ -78,7 +63,6 @@ const getValueLeft = async () => {
   output_text.innerText = `1 ${`${mezsag}`}= ${1/out}   ${mez}  `
 }
 
-
 const getValueRight = async () => {
 
   const response = await fetch(`https://api.exchangerate.host/latest?base=${mez}&symbols=${mezsag}`);
@@ -86,7 +70,4 @@ const getValueRight = async () => {
 
   let out = data.rates[`${mezsag}`]
   inputs.value = outputs.value / out;
-
-
-
 }
